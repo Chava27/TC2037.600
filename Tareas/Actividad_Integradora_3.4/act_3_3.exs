ccd1f112738ada16527a6fbb40133498ecb71ec7
Salvador Salgado <chavasalgado27@gmail.com>
2022-05-18 19:51:59 -0500

    update

--- Tareas/Actividad_Integradora_3.4/act_3_3.exs
+++ Tareas/Actividad_Integradora_3.4/act_3_3.exs
@@ -2,6 +2,15 @@
 #
 #
 #
+# ("\w+" *:)                   "movie":
+# (: *"[a-zA-z0-9.: ]+ *")     "Merian C. Cooper"
+# ( *"\w+" *)(:)( *"[a-zA-z0-9.: ]+ *")(,)
+# ( *"\w+" *)(:)
+# REGEX for key: "movie":
+#"King Kong",
+# {,},[,],:,,
+# REGEX for symbols: ([{},:\[\]]+)
+# REGEX for { awkdakdw:: [{},:\[\]]+(?= *"\w+": *)
 
 
 defmodule Syntax do
@@ -10,12 +19,38 @@ defmodule Syntax do
     tokens =
       in_filename
       |> Enum.stream!() #genera lista de renglones
-      |> Enum.map()
-      |> Enum.joint("\n")
+      |> Enum.map(&token/1)
+      |> Enum.join("\n")
     File.write(out_filename,tokens)
   end
 
-  def token?(line) do
+  def token(line), do: token(line,"")
+  defp token(line,result) when line == "", do: result
+  defp token(line, result) do
+    cond do
+      Regex.match?(~r/ *"\w+" *:/, line) -> js_html(line, "k", result)
+      Regex.match?(~r/[{},:\[\]]+/, line) -> js_html(line, "p", result)
+    end
+
+
+    [key,dots,value,comma] = Regex.run
+    token()
+
+
+
+   # "<span class = \"token\"> #{name} <\span>"
 
   end
-end
+
+
+
+  def js_html(line, type, result)
+    cond do
+      type == "k" -> [token,key,dot]=Regex.run(~r/( *"\w+" *)(:)/,line)
+      #key = "movie"
+      #dot = ":"
+      type == "p" -> [token]=Regex.run(~r/([{},:\[\]]+)/, line)
+    end
+
+
+  end
