87fca7b114f9352debecebc74338bc74ca0b997c
Luisjakg <52958375+Luisjakg@users.noreply.github.com>
2022-06-10 17:10:32 -0500

    Update act_5_2.exs
    proyecto acabado :D

--- Tareas/Actividad_Primes/act_5_2.exs
+++ Tareas/Actividad_Primes/act_5_2.exs
@@ -5,35 +5,47 @@
 
 defmodule Hw.Primes do
 
-  def prime(limit) when limit == 1, do: nil
+  def prime(limit) when limit == 1, do: 0
   def prime(limit) when limit == 2, do: 2
-  def prime(limit), do: do_prime(floor(:math.sqrt(limit)),1)
-  defp do_prime(limit,count) when count==limit, do: limit
-  defp do_prime(limit,count) do
+  def prime(limit), do: do_prime(limit,ceil(:math.sqrt(limit)),2)
+  defp do_prime(limit,stop,count) when count==stop + 1, do: limit
+  defp do_prime(limit,stop,count) do
     cond do
-      IO.puts "entrando al comparador"
       rem(limit,count) === 0 -> do_prime(limit)
-      true -> do_prime(limit,count+1)
+      true -> do_prime(limit,stop,count+1)
     end
   end
   defp do_prime(_limit), do: 0
 
-  def sum_primes(limit) do
-  limit =  floor(:math.sqrt(limit))
-  1..limit
+  def sum_primes(limit), do: sum_primes(0,limit)
+  def sum_primes(start, limit) do
+  start..limit
   |> Enum.map(&prime(&1))
-  |> Enum.join()
+  |> IO.inspect
+  |> Enum.sum()
   end
-
   def sum_primes_parallel(limit) do
     threads = System.schedulers()
-    sum_primes_parallel(limit, threads)
+    cond do
+      threads > limit -> sum_primes_parallel(limit, limit)
+      true ->     sum_primes_parallel(limit, threads)
+    end
   end
+
   def sum_primes_parallel(limit,threads) do
-    IO.puts "MAIN THREAD END"
+    range = div(limit, threads)
+    IO.inspect range
+    residue = limit - range * threads
+    IO.inspect residue
+    ranges = Enum.map(1..threads, fn x -> x*range end)
+    last_element = List.last(ranges)
 
-
-    IO.puts "MAIN THREAD START"
+    result = ranges
+    |> IO.inspect
+    |> Enum.map(&Task.async(fn -> sum_primes(&1 - range + 1, &1) end))
+    |> Enum.map(&Task.await/1)
+    |> Enum.sum()
+    result + sum_primes(last_element, last_element + residue)
   end
 
-end
+  end
